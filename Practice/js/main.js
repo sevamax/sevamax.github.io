c0c089cd3aa6696d0a65d79bcc5f70349d9b2a79
sevamax <seva_max2000@ukr.net>
2020-08-18 16:05:45 +0300

    minjs

--- Practice/js/main.js
+++ Practice/js/main.js
@@ -17,8 +17,8 @@
                   breakpoint: 767,
                   settings: {
                     slidesToShow: 1,
-                    prevArrow: '<button class="prev"></button>',
-                    nextArrow: '<button class="next"></button>',
+                    prevArrow: '<button class="arrow prev"></button>',
+                    nextArrow: '<button class="arrow next"></button>',
                     slidesToScroll: 1
                   }
                 }
